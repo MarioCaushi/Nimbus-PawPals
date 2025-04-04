e117bac7493ddee866edba0fc4175aa323cac0cd
MarioCaushi <caushimario321@gmail.com>
2025-04-05 01:31:26 +0200

    Finished integration of not-logged in page

--- Front-End/src/components/Products/ProductFilter.jsx
+++ Front-End/src/components/Products/ProductFilter.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 
-const ProductFilter = ({ types }) => {
+const ProductFilter = ({ types, handleApplyFilters }) => {
 
     return (
         <form className="p-4 border rounded shadow" style={{ backgroundColor: '#f8faff' }}>
             {/* Category filter */}
             <div className="mb-4">
                 <label htmlFor="category" className="form-label" style={{ color: '#606060' }}>Category:</label>
-                <select id="category" name="category" className="form-select shadow-sm">
+                <select id="category" name="category" className="form-select shadow-sm"
+                value={types.category} onChange={(e) => handleApplyFilters(e, 'category', e.target.value)}>
                     <option value="">Select</option>
-                    {types && types["categories"] && types["categories"].map(product => {
+                    {types && types["Products"]["categories"] && types["Products"]["categories"].map(product => {
                         return <option key={product} value={product}>{product}</option>;
                     })}
                 </select>
@@ -18,22 +19,24 @@ const ProductFilter = ({ types }) => {
             <div className="mb-4">
                 <label className="form-label">Price Range:</label>
                 <div className="input-group shadow-sm">
-                    <input type="number" name="minPrice" placeholder="Min Price" className="form-control" />
-                    <input type="number" name="maxPrice" placeholder="Max Price" className="form-control" />
+                    <input type="number" name="minPrice" placeholder="Min Price" className="form-control" value={types.minPrice} onChange={(e) => handleApplyFilters(e,"minPrice", e.target.value)}/>
+                    <input type="number" name="maxPrice" placeholder="Max Price" className="form-control" value={types.maxPrice} onChange={(e) => handleApplyFilters(e,"maxPrice", e.target.value)} />
                 </div>
             </div>
             {/* Animal type filter */}
             <div className="mb-4">
                 <label htmlFor="animalType" className="form-label">Animal Type:</label>
-                <select id="animalType" name="animalType" className="form-select shadow-sm">
+                <select id="animalType" name="animalType" className="form-select shadow-sm"
+                value={types.animalType} onChange={(e) => handleApplyFilters(e, 'animalType', e.target.value)}>
                     <option value="">Select</option>
-                    {types && types["animalTypes"] && types["animalTypes"].map(product => {
+                    {types && types["Products"]["animalTypes"] && types["Products"]["animalTypes"].map(product => {
                         return <option key={product} value={product}>{product}</option>;
                     })}
                 </select>
             </div>
             {/* Submit button */}
-            <button type="submit" className="btn btn-primary shadow-sm" style={{ width: '100%', background: 'linear-gradient(to right, #33ccff, #ff99cc)' }}>Apply Filters</button>
+            <button type="submit" className="btn btn-primary shadow-sm" style={{ width: '100%', background: 'linear-gradient(to right, #33ccff, #ff99cc)' }}
+            onClick={(e) => handleApplyFilters(e, "ApplyFilters",null)}>Apply Filters</button>
         </form>
     );
 };
--- Front-End/src/pages/Products.jsx
+++ Front-End/src/pages/Products.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
 import NavigationBar from '../components/NavBars/NavigationBar';
 import ProductView from '../components/Products/ProductView';
 
@@ -9,7 +8,6 @@ const ROLE_KEY = 'role';
 const Products = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [role, setRole] = useState('');
-  const [products, setProducts] = useState({ Products: [], Types: [] });
 
   useEffect(() => {
     const token = localStorage.getItem(LOGGED_IN_KEY);
@@ -18,40 +16,12 @@ const Products = () => {
       setLoggedIn(true);
       setRole(savedRole);
     }
-
-    getAPI();
   }, []);
-
-  const getAPI = async () => {
-    try {
-      const response1 = await axios.get('http://localhost:5067/api/Product');
-      let types = []; // Default empty array if the second call fails
-      if (response1.status === 200) {
-        const response2 = await axios.get('http://localhost:5067/api/Product/types');
-        if (response2.status === 200) {
-          types = response2.data;
-        } else {
-          console.error("Error fetching product types from API");
-        }
-        setProducts({
-          Products: response1.data,
-          Types: types
-        });
-      }
-    } catch (error) {
-      console.error("Error fetching products from API", error);
-    }
-  };
-
-  // Reset function adjusted to match defined state
-  const reset = () => {
-    setProducts({ Products: [], Types: [] });
-  };
 
   return (
     <>
       <NavigationBar loggedIn={loggedIn} role={role} />
-      <ProductView loggedIn={loggedIn} role={role} products={products} />
+      <ProductView loggedIn={loggedIn} role={role} />
     </>
   );
 };
